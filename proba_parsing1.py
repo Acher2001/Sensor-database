1e088c570b7b4ee620bfadab8e928fee5953a69d
Sasha <sasha.festus2@gmail.com>
2024-03-20 14:57:03 +0500

    Список создаётся, но цена не получяется.

--- proba_parsing1.py
+++ proba_parsing1.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Функция для обработки товаров на странице
 def process_products(page_source, file):
@@ -15,23 +18,19 @@ def process_products(page_source, file):
         return
 
     for product_div in product_divs:
-        product_name_tag = product_div.find('a')
-        if product_name_tag:
-            product_name = product_name_tag.text.strip()
-            exists_tag = product_div.find_next_sibling('span', {'field': 'exists'})
-            if exists_tag and "Товар в наличии" in exists_tag.text:
-                price_span = product_div.find_next_sibling('div', {'class': 'price'})
-                if price_span:
-                    price = price_span.text.strip()
-                else:
-                    price = "Цена не указана"
+        product_name = product_div.text.strip()
+        exists_tag = product_div.find_next_sibling('span', {'field': 'exists'})
+        if exists_tag and "Товар в наличии" in exists_tag.text:
+            price_span = product_div.find_next_sibling('div', {'class': 'price'})
+            if price_span:
+                price = price_span.text.strip()
+            else:
+                price = "Цена не указана"
 
-                file.write("Название товара: {}\n".format(product_name))
-                file.write("Наличие: {}\n".format(exists_tag.text.strip()))
-                file.write("Цена: {}\n".format(price))
-                file.write("-" * 50 + "\n")
-        else:
-            file.write("На этой странице есть товары, но их название не найдено.\n")
+            file.write("Название товара: {}\n".format(product_name))
+            file.write("Наличие: {}\n".format(exists_tag.text.strip()))
+            file.write("Цена: {}\n".format(price))
+            file.write("-" * 50 + "\n")
 
 # Настройка веб-драйвера Chrome
 service = Service(ChromeDriverManager().install())
@@ -49,7 +48,13 @@ with open("output.txt", "w", encoding="utf-8") as file:
     for url in urls:
         driver.get(url)
         file.write("Страница: {}\n".format(url))
+        file.write("=" * 50 + "\n")
+
+        # Даем время на загрузку динамического контента
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[field="link"]')))
+
         process_products(driver.page_source, file)
+        file.write("\n")
 
 # Завершаем сеанс браузера
 driver.quit()
